f94da71460b8a8af0978db422af7ef3211e2da87
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-25 21:58:14 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
+let distanz = 0
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, 180)
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 0)
 })
+function messe_distanz () {
+    distanz = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.pause(500)
+    if (distanz < 4) {
+        pins.servoWritePin(AnalogPin.P0, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P0, 0)
+    }
+}
 basic.forever(function () {
 	
 })
